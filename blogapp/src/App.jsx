3a50e5e09b6ee1944d83fa3fef6101d4d9c77596
Sumit6307 <sumitkesar6307@gmail.com>
2024-10-29 12:25:56 +0530

    A Blog App

---
+++ blogapp/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Gravity Blog</h1>
+          <h2>A blog Website</h2>
+      <h3>Blog write by Sumit</h3>
+       <h4>Write , Update and Swing</h4>
+       <h5>Sponser by HashNode</h5>
+        <h6> @ Copyright reserved 2024</h6>
+    </>
+  )
+}
+
+export default App
